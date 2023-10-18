cf2bac3d1cc619e42b0e2db5402c9d53b822d9b8
Jeranguz <JeremyGD997@hotmail.com>
2023-10-18 10:55:42 -0600

    Category navigation bar created, main.js created, and adjustments in the other index

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const swiper = new Swiper(".swiper-hero", {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 55,
+   loopAdditionalSlides:5,
+   parallax: true,
+    
+    loop: true,
+
+    autoplay: {
+      delay: 1500,
+
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints:{
+
+      680: {
+        slidesPerView: 1,
+        spaceBetween:20, 
+    },
+
+      950: {
+        slidesPerView: 2,
+        spaceBetween:20, 
+
+      },
+
+    1600: {
+      slidesPerView: 3,
+        spaceBetween:20, 
+    },
+
+    },
+
+  });
